684f0ad6fabb2c1e3f954844f227b87a56a9e8e8
PatHeydasch <55458892+PatHeydasch@users.noreply.github.com>
2020-01-22 14:03:04 -0500

    Update myScript.js

--- tests/myScript.js
+++ tests/myScript.js
@@ -1,12 +1,8 @@
 function myLoop(){
-var end = document.getElementById("end").value;
-for(var i = 0; i <= end; i++)
+var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+for(var i = start; i <= end; i++)
   {
     document.write('<p>' + i + '</p>');
   }
 }
-document.getElementById("start").value;
-for(var i = "start"; i <= end; i++)
-  {
-    document.write('<p>' + i + '</p>');
-  }
